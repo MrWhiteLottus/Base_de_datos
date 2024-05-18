f94114bffd2f21d5fa049c432a606c398bc5005b
MrWhiteLottus <aguilar.tapia.benjamin@gmail.com>
2024-05-18 01:08:40 -0400

    act

--- ejercicios/ejercicio de record.sql
+++ ejercicios/ejercicio de record.sql
@@ -22,7 +22,30 @@ SELECT
 FROM cliente
 );
 
-TYPE rec_informe is RECORD 
+TYPE rec_informe is RECORD(
+    v_tipo varchar2(20),
+    v_id_vendedor vendedor.id_vendedor%type,
+    v_nombre_completo varchar2(150),
+    v_rut_vendedor vendedor.rut_vendedor%type,
+    v_nom_grupo grupo.nom_grupo%type,
+    v_nom_categoria categoria.nom_categoria%type
+);
 
 BEGIN
+OPEN cur_vendedor;
+
+    
+    FOR rec_informe IN cur_vendedor LOOP
+        
+        DBMS_OUTPUT.PUT_LINE('Tipo: ' || rec_informe.v_tipo);
+        DBMS_OUTPUT.PUT_LINE('ID Vendedor: ' || rec_informe.v_id_vendedor);
+        DBMS_OUTPUT.PUT_LINE('Nombre Completo: ' || rec_informe.v_nombre_completo);
+        DBMS_OUTPUT.PUT_LINE('RUT Vendedor: ' || rec_informe.v_rut_vendedor);
+        DBMS_OUTPUT.PUT_LINE('Nombre Grupo: ' || rec_informe.v_nom_grupo);
+        DBMS_OUTPUT.PUT_LINE('Nombre Categoria: ' || rec_informe.v_nom_categoria);
+        DBMS_OUTPUT.PUT_LINE('---------------------------------');
+    END LOOP;
+
+
+
 END;
